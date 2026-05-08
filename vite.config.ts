ab058c2a1f3f11f9da51c16be1bb694b8537a970
Karolis M <karolis.muliuolis@teltonika.lt>
2026-05-08 11:56:43 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,7 @@ export default defineConfig({
     "*": "vp fmt --write",
   },
   lint: { options: { typeAware: true, typeCheck: false } },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+  },
 });
